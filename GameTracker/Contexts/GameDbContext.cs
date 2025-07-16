06477ad699c2be229f247b6df79de53dafbcf696
Vaziroon10 <nagimovvazir@gmail.com>
2025-07-16 18:26:29 +0500

    Кастомый контекст

---
+++ GameTracker/Contexts/GameDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GameTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameTracker.Contexts
+{
+    internal class GameDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+
+        public GameDbContext(DbContextOptions<GameDbContext> options) : base(options)
+        {
+        }
+    }
+}
